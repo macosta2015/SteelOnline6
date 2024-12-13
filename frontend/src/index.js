c2dff9c20c46a592e54650beebbf1f8431a520bb
macosta2015 <macosta2015@my.fit.edu>
2024-12-12 23:08:39 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -30,7 +30,6 @@ function Main() {
       {/* DisplayEmails will re-fetch whenever emailRefreshTrigger changes */}
       <DisplayEmails emailRefreshTrigger={emailRefreshTrigger} />
       <EmailManager /> Render the new combined EmailManager component
-      <EmailManager /> Render the new combined EmailManager component
 
     </>
   );
